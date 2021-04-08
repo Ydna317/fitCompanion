f16b31ee1810d13d859d9dc807705b139a6bb469
Andy Nguyen <Ydna317@gmail.com>
2021-04-07 20:07:46 -0700

    complete and submit buttons not working.

--- routes/api.js
+++ routes/api.js
@@ -1,76 +1,76 @@
-// const router = require('express').Router();
-// const Workout = require('../models/workout.js');
+const router = require('express').Router();
+const Workout = require('../models/workout.js');
 
-// router.post('/api/workouts', (req, res) => {
-//   Workout.create({})
-//     .then((dbWorkout) => {
-//       res.json(dbWorkout);
-//     })
-//     .catch((err) => {
-//       res.json(err);
-//     });
-// });
+router.post('/api/workouts', (req, res) => {
+  Workout.create({})
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
 
-// router.put('/api/workouts/:id', ({ body, parameters }, res) => {
-//   Workout.findByIdAndUpdate(
-//     parameters.id,
-//     { $push: { exercises: body } }
-//   )
-//     .then((dbWorkout) => {
-//       res.json(dbWorkout);
-//     })
-//     .catch((err) => {
-//       res.json(err);
-//     });
-// });
+router.put('/api/workouts/:id', ({ body, parameters }, res) => {
+  Workout.updatebyID(
+    parameters.id,
+    { $push: { exercises: body } }
+  )
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
 
-// router.get('/api/workouts', (req, res) => {
-//   Workout.combinedWorkouts([
-//     {
-//       $addFields: {
-//         totalDuration: {
-//           $sum: '$exercises.duration',
-//         },
-//       },
-//     },
-//   ])
-//     .then((dbWorkouts) => {
-//       res.json(dbWorkouts);
-//     })
-//     .catch((err) => {
-//       res.json(err);
-//     });
-// });
+router.get('/api/workouts', (req, res) => {
+  Workout.aggregate([
+    {
+      $addFields: {
+        totalDuration: {
+          $sum: '$exercises.duration',
+        },
+      },
+    },
+  ])
+    .then((dbWorkouts) => {
+      res.json(dbWorkouts);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
 
-// router.get('/api/workouts/range', (req, res) => {
-//   Workout.combinedWorkouts([
-//     {
-//       $addFields: {
-//         totalDuration: {
-//           $sum: '$exercises.duration',
-//         },
-//       },
-//     },
-//   ])
-//     .sort({ _id: -1 })
-//     .limit(7)
-//     .then((dbWorkouts) => {
-//       console.log(dbWorkouts);
-//       res.json(dbWorkouts);
-//     })
-//     .catch((err) => {
-//       res.json(err);
-//     });
-// });
+router.get('/api/workouts/range', (req, res) => {
+  Workout.aggregate([
+    {
+      $addFields: {
+        totalDuration: {
+          $sum: '$exercises.duration',
+        },
+      },
+    },
+  ])
+    .sort({ _id: -1 })
+    .limit(7)
+    .then((dbWorkouts) => {
+      console.log(dbWorkouts);
+      res.json(dbWorkouts);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
 
-// router.delete('/api/workouts', ({ body }, res) => {
-//   Workout.deletebyID(body.id)
-//     .then(() => {
-//       res.json(true);
-//     })
-//     .catch((err) => {
-//       res.json(err);
-//     });
-// });
+router.delete('/api/workouts', ({ body }, res) => {
+  Workout.deletebyID(body.id)
+    .then(() => {
+      res.json(true);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
 
-// module.exports = router;
+module.exports = router;
--- server.js
+++ server.js
@@ -2,14 +2,30 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+
 // creating port
 const PORT = 3003;
+
 // creating express variable
 const app = express();
+
+// app.use for logger, express and mongoose connects
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
 // getting routes
 app.use(require("./routes/api.js"));
 app.use(require("./routes/routes.js"));
 
 app.listen(PORT, () => {
-    console.log(`App running on port ${PORT}!`);
+    console.log(`Open up localhost ${PORT}!`);
   });
